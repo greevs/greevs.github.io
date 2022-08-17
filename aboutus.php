a0a7f6a59242d93b012d20ddbbb1d34b43560af1
greevs <111432659+greevs@users.noreply.github.com>
2022-08-17 17:07:42 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+				.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+ #nope {
+
+ 	color:white;
+ }
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+
+
+
+		body {
+      background-image: url(image1.jpg);
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+
+html {
+  box-sizing: border-box;
+  background-color: black;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+.column {
+  float: left;
+  width: 33.3%;
+  margin-bottom: 16px;
+  padding: 0 8px;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  margin: 8px;
+}
+
+.about-section {
+  padding: 50px;
+  text-align: center;
+  background-color: #474e5d;
+  color: white;
+}
+
+.container {
+  padding: 0 16px;
+}
+
+.container::after, .row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.title {
+  color: grey;
+}
+
+.button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+}
+
+.button:hover {
+  background-color: #555;
+}
+
+@media screen and (max-width: 650px) {
+  .column {
+    width: 100%;
+    display: block;
+  }
+}
+	</style>
+	<title>About us</title>
+</head>
+<body>
+
+	<title>Our Team</title>
+</head>
+<body bgcolor="black">
+	</div>
+	
+
+<div class="topnav">
+  <a class="active" href="homepage.php">Home</a>
+  <a href="faq.php">FAQ</a>
+  <a href="aboutus.php">about us</a>
+  <a href="login.php">Login/Register</a>
+  <a href="test2.php">Subscriptions</a>
+</div>
+
+
+<div class="about-section">
+  <h1>Our Team </h1>
+</div>
+	<div id="nope">
+<h2 style="text-align:center"></h2>
+<div class="row">
+ </div>
+        
+      </div>
+    </div>
+  </div>
+	<div id="nope">
+  <div class="column">
+    <div class="card">
+      
+      <img src="6.jpeg" style="width:100%" height="500">
+      <div class="container">
+        <h2>Jasreen Bindra</h2>
+        <p class="title">CEO</p>
+        <p></p>
+        <p></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+     	<div id="nope">
+      <img src="5.jpeg" style="width:100%" height="500">
+      <div class="container">
+        <h2>Naina Yadav</h2>
+        <p class="title">CFO</p>
+        <p></p>
+        <p></p>
+      </div>
+  </div>
+    </div>
+  </div>
+  <div class="row">
+  <div class="column">
+    <div class="card">
+     	<div id="nope">
+      <img src="4.jpeg" style="width:100%" height="500">
+      <div class="container">
+        <h2>Kritika Kandpal</h2>
+        <p class="title">Managing Director</p>
+        </div>
+      </div>
+    </div>
+</div>
+</div>
+    </div>
+</div>
+  </div>
+
+</body>
+</html>
